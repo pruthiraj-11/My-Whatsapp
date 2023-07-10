7387e615d8d25d80041756ef88eabd76606b1a1d
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-07-10 14:37:31 +0530

    My Whatsapp

--- app/src/main/java/com/example/whatsapp/SplashScreen.java
+++ app/src/main/java/com/example/whatsapp/SplashScreen.java
@@ -1,39 +1,37 @@
 package com.example.whatsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         SharedPreferences sharedPreferences=getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
         boolean flag=sharedPreferences.getBoolean("switchstate",false);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if(flag==true){
-                    startActivity(new Intent(SplashScreen.this,BiometricAuthenticationActivity.class));
-                    finish();
-                }
-                else {
-                    startActivity(new Intent(SplashScreen.this,PhoneNumberAuthActivity.class));
-                    finish();
-                }
+        new Handler().postDelayed(() -> {
+            if(flag){
+                startActivity(new Intent(SplashScreen.this,BiometricAuthenticationActivity.class));
+                finish();
+            }
+            else {
+                startActivity(new Intent(SplashScreen.this,PhoneNumberAuthActivity.class));
+                finish();
             }
         }, 5000);
 
